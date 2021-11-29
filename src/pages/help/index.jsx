7185726a0e00fb76f29d42a5629538a47403f7ec
Fahad Sarwar <fahad.sarwar@zeninternet.co.uk>
2021-11-29 19:51:40 +0000

    Created help page to display youtube videos

--- src/pages/help/index.jsx
+++ src/pages/help/index.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useSession } from "next-auth/client";
+import { CircularProgress } from "@mui/material";
 
-const Help = () => (
-  <div>
-    Help
-  </div>
-);
+const Help = () => {
+  const [session, loading] = useSession();
+
+  if (loading) {
+    return <CircularProgress />;
+  }
+
+  if (!session) {
+    return <CircularProgress />;
+  }
+
+  return (
+    <div>
+      <div>Help</div>
+    </div>
+  );
+};
 
 export default Help;
